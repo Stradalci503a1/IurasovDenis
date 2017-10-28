e8bc8a2c44490c796e410b058129d831af597fdf
KingEnderBrine <31794213+KingEnderBrine@users.noreply.github.com>
2017-10-28 20:46:03 +0700

    Deleted "main"

--- lab05/src/Formatter.java
+++ lab05/src/Formatter.java
@@ -1,8 +1,5 @@
 public class Formatter {
-    public static void main(String[] args) {
-        String s = build("Hello {0}, I am {1} years old", "hell", 15);
-        System.out.println(s);
-    }
+
     public static String build(String formatString, Object... arguments){
 
         String output = formatString.toString();
