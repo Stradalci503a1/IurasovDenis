f2ce811bb9e2c0f303832f2cc04e953839c2cd5e
KingEnderBrine <31794213+KingEnderBrine@users.noreply.github.com>
2017-11-18 23:01:16 +0700

    Copypaste lab04

--- lab04/src/FileAnalizer.java
+++ lab04/src/FileAnalizer.java
@@ -1,117 +1,195 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
+//Letters - "[a-zA-Zа-яА-Я]"
+//Words - "([^A-Za-zА-Яа-я])([\\s\\W]+)"
+
+import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.attribute.FileTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileAnalizer {
 
-    private Path path;
-    private int wordsCount;
-    private int lettersCount;
-    private int linesCount;
-    private Map<Character, Integer> symbolsCount;
-    private Map<String, FileTime> lastUpdateTime;
+    private static ArrayList<Container> cache = new ArrayList<Container>();
+    private static final Pattern wordsPattern = Pattern.compile("[\\s]+", Pattern.DOTALL);
 
-    public FileAnalizer(String path){
-        this.path = Paths.get(path);
-        symbolsCount = new HashMap<>();
-        lastUpdateTime = new HashMap<>();
-        wordsCount = 0;
-        lettersCount = 0;
-        linesCount = 0;
+    private String path;
+    private Container container;
+
+    public String getPath() {
+        return path;
     }
 
-    public int countLetters(){
-        if (fileWasModified("letters")) {
-            try {
-                BufferedReader content = Files.newBufferedReader(path);
-                lettersCount = 0;
-                while (content.ready()) {
-                    lettersCount += content.readLine().replaceAll("[^a-zA-Zа-яА-я]", "").length();
+    public FileAnalizer(String path) {
+
+        if (null == path) {
+            throw new NullPointerException("Nullable path");
+        }
+
+        this.path = path;
+
+        for (Container container : cache) {
+            if (path.equals(container.path)) {
+                this.container = container;
+                break;
+            }
+        }
+
+        if (null == this.container) {
+
+            container = new Container(path);
+            cache.add(container);
+        }
+
+    }
+
+    public int countLetters() {
+
+        if (-1 == container.countLetters) {
+
+            int count = 0;
+            HashMap<Character, Integer> statistic = container.refreshStatistic();
+            for (Character symbol : statistic.keySet()) {
+                if (Character.isLetter(symbol)) {
+                    count += statistic.get(symbol);
                 }
-                content.close();
-            } catch (IOException e) {
-                return -1;
             }
+
+            container.countLetters = count;
+        }
+        else {
+            container.refreshStatistic();
         }
 
-        return lettersCount;
+        return container.countLetters;
     }
 
-    public int countWords(){
-        if (fileWasModified("words")){
+    public int countWords() {
+        container.refreshStatistic();
+
+        return container.countWords;
+    }
+
+    public int countLines() {
+        container.refreshStatistic();
+
+        return container.countLines;
+    }
+
+    public Map<Character, Integer> countFrequencyCharacteristic() {
+        return container.refreshStatistic();
+    }
+
+    private class Container {
+
+        public final String path;
+        public long timestamp;
+
+        public HashMap<Character, Integer> statistic;
+        public int countWords;
+        public int countLetters;
+        public int countLines;
+
+        private final File file;
+
+        public Container(String path) {
+
+            this.path = path;
+            this.file = new File(path);
+            this.timestamp = file.lastModified();
+
             try {
-                BufferedReader content = Files.newBufferedReader(path);
-                wordsCount = 0;
-                while (content.ready()) {
-                    wordsCount += content.readLine().replaceAll("[^a-zA-Zа-яА-я ]", "").split("[ ]+").length;
+                String type = Files.probeContentType(file.toPath());
+                if (!"text/plain".equals(type)) {
+                    throw new IllegalArgumentException("Bad file type.");
                 }
-                content.close();
-            } catch (IOException e) {
-                return -1;
+            } catch (IOException ioException) {
+                throw new IllegalArgumentException("Can't check file extension.", ioException);
             }
+
+            this.statistic = null;
+            this.countWords = -1;
+            this.countLetters = -1;
+            this.countLines = -1;
         }
 
-        return wordsCount;
-    }
+        public boolean isModified() {
 
-    public int countLines(){
-        if (fileWasModified("lines")){
+            File file = new File(path);
+            return timestamp != file.lastModified();
+        }
+
+        public HashMap<Character, Integer> refreshStatistic() {
+
+            if (null == statistic) {
+                analyze();
+            }
+            else if(isModified()) {
+                statistic.clear();
+                statistic = null;
+
+                analyze();
+            }
+
+            return statistic;
+        }
+
+        private void analyze() {
+
+            this.countWords = 0;
+            this.countLines = 0;
+            this.statistic = new HashMap<Character, Integer>();
+
             try {
-                BufferedReader content = Files.newBufferedReader(path);
-                linesCount = 0;
-                while (content.ready()) {
-                    content.readLine();
-                    linesCount++;
+                FileInputStream stream = new FileInputStream(path);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+
+                    this.countWords += countWordsIn(line);
+                    this.countLines ++;
+                    countCharactersTo(this.statistic, line);
                 }
-                content.close();
-            } catch (IOException e) {
-                return -1;
+
+                reader.close();
+            } catch (FileNotFoundException notFound) {
+                throw new IllegalArgumentException("File not found.", notFound);
+            } catch (IOException ioException) {
+                throw new IllegalArgumentException("Problem with read file's content.", ioException);
             }
         }
 
-        return linesCount;
-    }
+        private int countWordsIn(String line) {
 
-    public Map<Character,Integer> countFrequencyCharacteristic(){
-        if (fileWasModified("symbols")) {
-            try {
-                BufferedReader content = Files.newBufferedReader(path);
-                Map<Character, Integer> symbolsCount = new HashMap<>();
+            int result = 0;
 
-                while (content.ready()) {
-                    for (char symbol : content.readLine().toCharArray()) {
-                        if (symbolsCount.containsKey(symbol)) {
-                            symbolsCount.put(symbol, symbolsCount.get(symbol) + 1);
-                        } else {
-                            symbolsCount.put(symbol, 1);
-                        }
-                    }
+            Matcher matcher = wordsPattern.matcher(line);
+            while (matcher.find()) {
+                result++;
+            }
+
+            if (!"".equals(line)) {
+                result++;
+            }
+
+            return result;
+        }
+        private void countCharactersTo(HashMap<Character, Integer> statistic, String line) {
+
+            StringBuilder builder = new StringBuilder(line);
+            builder.append('\n');
+
+            for (Character symbol : builder.toString().toCharArray()) {
+
+                Integer count = statistic.get(symbol);
+                if (count != null) {
+                    statistic.put(symbol, count + 1);
+                } else {
+                    statistic.put(symbol, 1);
                 }
-                this.symbolsCount.putAll(symbolsCount);
-            } catch (IOException e) {
-                return null;
             }
         }
-
-        return symbolsCount;
-    }
-
-    private boolean fileWasModified(String key){
-        try {
-            if (!Files.getLastModifiedTime(path).equals(lastUpdateTime.get(key))){
-                lastUpdateTime.put(key, Files.getLastModifiedTime(path));
-                return true;
-            }
-        }
-        catch (IOException e){
-            return true;
-        }
-        return false;
     }
 }
