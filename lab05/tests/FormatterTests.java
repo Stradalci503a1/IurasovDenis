247e0bd757d83bd706be55be7ebd4ae57b57acb7
KingEnderBrine <31794213+KingEnderBrine@users.noreply.github.com>
2017-11-18 13:04:21 +0600

    Some changes

--- lab05/tests/FormatterTests.java
+++ lab05/tests/FormatterTests.java
@@ -14,6 +14,11 @@ public class FormatterTests {
     }
 
     @Test
+    public void classArgument(){
+        TestTask testTask = new TestTask(5, 7);
+        check("dsgsd {0} sdgsd", "dsgsd A: 5 B: 7 sdgsd", testTask);
+    }
+    @Test
     public void incorrectFiller() {
         check("342 {a} rwq {}", "342 {a} rwq {}");
     }
@@ -29,11 +34,31 @@ public class FormatterTests {
     }
 
     @Test
+    public void reverseArguments(){
+        check("hbdf {1} gsd gs {0} gsd", "hbdf Arg2 gsd gs Arg1 gsd", "Arg1", "Arg2");
+    }
+    
+    @Test
     public void correct(){
         check("Hello {0}, I am {1} years old","Hello hell, I am 15 years old", "hell", 15);
     }
-
+    
     private void check(String input, String output, Object... arguments){
         Assert.assertEquals(Formatter.build(input, arguments), output);
+    }
+    
+    private class TestTask{
+        private int a;
+        private int b;
+        
+        public TestTask(int a, int b){
+            this.a = a;
+            this.b = b;
+        }
+        
+        @Override
+        public String toString(){
+            return "A: " + a + " B: " + b;
+        }
     }
 }
