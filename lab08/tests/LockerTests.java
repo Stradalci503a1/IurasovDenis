82811704f8e3a7a40dd53e3b85cf183c16553c8e
KingEnderBrine <31794213+KingEnderBrine@users.noreply.github.com>
2017-11-25 11:51:07 +0600

    Update LockerTests.java

--- lab08/tests/LockerTests.java
+++ lab08/tests/LockerTests.java
@@ -4,23 +4,27 @@ import org.junit.Test;
 public class LockerTests {
 
     @Test
-    public void changeLocked(){
-        Locker locker = new Locker(5, true);
+    public void check() throws LockerLockedException {
+
+        Locker locker = new Locker();
+        locker.unlock();
+
+        //Set and get value
+        locker.set(15);
+        Assert.assertEquals(15, locker.get());
+
+        //Lock and change
+        locker.lock();
         try {
             locker.set(3);
             Assert.fail("Locker locked but value changed");
         } catch (LockerLockedException exception) {
+            Assert.assertEquals(15, locker.get());
         }
-    }
 
-    @Test
-    public void changeUnlocked(){
-        Locker locker = new Locker(3, false);
-        try{
-            locker.set(7);
-            Assert.assertEquals(7, locker.get());
-        } catch (LockerLockedException exception){
-            Assert.fail("Locker unlocked but value not changed");
-        }
-    }
+        //Unlock and change
+        locker.unlock();
+        locker.set(23);
+        Assert.assertEquals(23, locker.get());
+}
 }
